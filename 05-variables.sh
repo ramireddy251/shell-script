f5c7a4ec5326f26a116b14f580e3606b818b463f
ram <ramireddy251@gmail.com>
2026-01-28 18:53:19 +0530

    added variables file

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+START_TIME=$(date +%s)
+echo "script executed at $START_TIME"
+
+sleep 10
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "script executed at $TOTAL_TIME"
+
+
