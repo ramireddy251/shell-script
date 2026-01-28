9ea47ca19ba2e661d651da644a2e6a160589836e
ram <ramireddy251@gmail.com>
2026-01-28 19:15:57 +0530

    added variables file

---
+++ 06-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+##### Special Variables #######
+echo "All args passed to the script: $@"
+echo "Number of args passed to the script: $#"
+echo "Script name: $0"
+echo "Present directory: $PWD"
+echo "Who is running: $USER"
+echo "Home directory of the script $HOME"
+echo "PID of the script: $$"
+sleep 100 &
+echo "PID of recently executed baground script: $!"
+echo "All args passed to the script: $*"
