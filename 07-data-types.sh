784fc8ef194b8bd3414692903875a829e5d1efad
ram <ramireddy251@gmail.com>
2026-01-28 20:06:06 +0530

    added data-types file

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -6,3 +6,11 @@ NUM2=200
 SUM=$(($NUM1+$NUM2))
 
 echo "sum is $SUM"
+
+#Array
+FRUITS=("Apple" "Banana" "Mango")
+
+echo "Fruits are: ${FRUITS[@]}"
+echo "First fruit is: ${FRUITS[0]}"
+echo "First fruit is: ${FRUITS[1]}"
+echo "First fruit is: ${FRUITS[2]}"
