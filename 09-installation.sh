2c2e13c4e9bbc50b4f78e1286bdceff92d01be52
ram <ramireddy251@gmail.com>
2026-01-28 21:44:53 +0530

    added installation file

--- 09-installation.sh
+++ 09-installation.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-apt install nginx -y
+USER_ID=$(id -u)
+
+if [ $USER_ID -eq 0 ]
+then
+  echo "Installing nginx"
+  apt install nginx -y
+  else
+    echo "Please run the script with root user access" 
+fi
