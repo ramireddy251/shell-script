37f22d86935550b85454ed157812b74b07d80ff1
ram <ramireddy251@gmail.com>
2026-01-28 15:38:49 +0530

    added variables file

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+read -p "Enter first Person name: " p1
+read -p "Enter second person name: " p2
+
+echo "$p1:: Hi $p2 How Are you?"
+echo "$p2:: Hi $p1 I am fine thanks for asking. How are you?"
+echo "$p1:: Doing fine. What are you learning $p2"
+echo "$p2:: I am learning shell shellscripting. what about you $p1"
