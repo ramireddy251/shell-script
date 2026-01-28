08b423dc347d220721d34935ab3237dd974cbbb0
ram <ramireddy251@gmail.com>
2026-01-28 16:05:56 +0530

    added variables file

---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+read -p "Enter User name: " user
+read -s -p "Enter Password: " password
+echo
+echo "--------------------------------"
+
+echo "Your name is $user"
+echo "Your password is $password"
