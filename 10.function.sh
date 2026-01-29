fcb202f79a558664c44535087c9ecca117b86f72
ram <ramireddy251@gmail.com>
2026-01-29 16:44:30 +0530

    added function file

---
+++ 10.function.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]
+then
+  echo "Please run the script with root user access"
+  exit 1
+  fi
+
+  VALIDATE () {
+     
+  if [ $1 -ne 0 ]
+  then
+    echo "$2 ..... Failure"
+    exit 1
+  else
+   echo "$2 ..... Success"
+fi   
+
+  }
+  apt install nginx -y
+  VALIDATE $? "Installing Nginx"
+
+
+apt install mysql-server -y
+VALIDATE $? "Installing mysql"  
+
+apt install nodejs -y
+VALIDATE $? "Installing Nodejs" 
+  
+
