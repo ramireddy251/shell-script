08cc8693d97d7448e4fb2f9eae3972f7be73ee9a
ram <ramireddy251@gmail.com>
2026-01-29 21:41:41 +0530

    added logs file

---
+++ 11.logs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USER_ID -ne 0 ]
+then
+  echo "Please run the script with root user access"
+  exit 1
+  fi
+
+  mkdir $LOGS_FOLDER
+
+  VALIDATE () {
+     
+  if [ $1 -ne 0 ]
+  then
+    echo "$2 ..... Failure"
+    exit 1
+  else
+   echo "$2 ..... Success"
+fi   
+
+  }
+  apt install nginx -y &>> $LOGS_FILE
+  VALIDATE $? "Installing Nginx"
+
+
+apt install mysql-server -y &>> $LOGS_FILE
+VALIDATE $? "Installing mysql"  
+
+apt install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Installing Nodejs" 
+  
+
