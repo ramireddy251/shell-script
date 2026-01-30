80f642cdbb97fc1b6655ac98a4306d85a5e40546
ram <ramireddy251@gmail.com>
2026-01-30 13:20:06 +0530

    added trap file

--- 18.trap.sh
+++ 18.trap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-set -e
+set -e # when it finds error it through the error to system in ERR
 
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
 echo "Hellow World"
 echo "I am learning shell"
 echoo "printing error here"
