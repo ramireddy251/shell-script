3636898fe1eabc740adb420082a771a7fe167332
ram <ramireddy251@gmail.com>
2026-01-31 16:11:48 +0530

    added arthematic-operation file

---
+++ arthematic-operation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Please enter a abd b values: " a b
+
+echo "Arthematic operations"
+
+echo "a+b is $(( a + b ))"
+echo "a-b is $(( a - b ))"
+echo "a*b is $(( a * b ))"
+echo "a/b is $(( a / b ))"
+echo "a%b is $(( a % b ))"
